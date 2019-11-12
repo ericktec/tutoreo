6f7b9f4d9af2626c2bb65d0cf926f1db478844fa
unknown <erick_tec@outlook.com>
2019-11-12 09:19:16 -0600

    firebase config

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAJAeeec9OxchMjIG1J1tXbR-n4AwjGuf0",
+    authDomain: "tutoreo-9b1d7.firebaseapp.com",
+    databaseURL: "https://tutoreo-9b1d7.firebaseio.com",
+    projectId: "tutoreo-9b1d7",
+    storageBucket: "tutoreo-9b1d7.appspot.com",
+    messagingSenderId: "341954460904",
+    appId: "1:341954460904:web:bbe6edd5007bacdfff5d5d"
+  };
+  firebase.initializeApp(firebaseConfig);
